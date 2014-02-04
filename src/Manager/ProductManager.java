d28df1fab694d2c9342c2cdf562f3a59a40520e0
Henrique Vicente <henriquevicente@gmail.com>
2014-02-04 02:34:00 -0300

    Adding the ProductManager
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

---
+++ src/Manager/ProductManager.java
@@ -0,0 +1,77 @@
+package Manager;
+
+import Entities.Product;
+import Exceptions.InvalidInputException;
+import Exceptions.MissingRolesException;
+import Exceptions.ObjectAlreadyExistsException;
+import Exceptions.ObjectNotFoundException;
+import Repos.Array.ProductRepoArray;
+
+public class ProductManager {
+    private ProductRepoArray repo;
+
+    public ProductManager(ProductRepoArray repo) {
+        this.repo = repo;
+    }
+
+    public void validate(int price, String name, String vendor, int amount) throws InvalidInputException {
+        if (amount < 1) {
+            throw new InvalidInputException("amount < 1");
+        }
+
+        if (price < 0) {
+            throw new InvalidInputException("price < 0");
+        }
+
+        if (name == null) {
+            throw new InvalidInputException("name");
+        }
+
+        if (vendor.length() < 1) {
+            throw new InvalidInputException("vendor");
+        }
+    }
+
+    public void validate(Product product) throws InvalidInputException {
+        this.validate(product.getPrice(), product.getName(), product.getVendor(), product.getAmount());
+    }
+
+    public void add(Product item)
+            throws ObjectAlreadyExistsException, InvalidInputException {
+        this.validate(item);
+
+        if (repo.has(item.getId())) {
+            throw new ObjectAlreadyExistsException();
+        }
+
+        repo.add(item);
+    }
+
+    public Product get(String id) throws ObjectNotFoundException {
+        Product product;
+
+        product = repo.get(id);
+
+        if (product == null) {
+            throw new ObjectNotFoundException();
+        }
+
+        return product;
+    }
+
+    public void update(String id)
+            throws ObjectNotFoundException, MissingRolesException, InvalidInputException {
+        Product product = this.get(id);
+
+        this.validate(product);
+
+    }
+
+    public void remove(String id) throws ObjectNotFoundException {
+        boolean removed = repo.remove(id);
+
+        if (!removed) {
+            throw new ObjectNotFoundException();
+        }
+    }
+}
