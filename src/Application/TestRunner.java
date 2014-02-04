77f7e7c8a691a36670ce075b64f2509c7bbd7e02
Henrique Vicente <henriquevicente@gmail.com>
2014-02-04 03:21:23 -0300

    Adding some stats to the test runner
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- src/Application/TestRunner.java
+++ src/Application/TestRunner.java
@@ -4,8 +4,16 @@ import Application.Tests.*;
 
 public class TestRunner {
     private Facade facade;
+    private int errors = 0;
+    private int total = 0;
 
     public void test(boolean result, String what) {
+        this.total += 1;
+
+        if (!result) {
+            this.errors += 1;
+        }
+
         System.out.println(what + ": " + ((result) ? "pass" : "fail"));
     }
 
@@ -15,11 +23,16 @@ public class TestRunner {
 
     public void run() {
         try {
-            System.out.println("inventory-control tests");
+            System.out.println("Running the inventory-control tests");
+
             new ActorTests(facade, this).runAll();
             new ProductPrototypeTests(facade, this).runAll();
             new ProductTests(facade, this).runAll();
             new OrderTests(facade, this).runAll();
+
+            System.out.print("\nTotal: " + this.total + "; Passing: " + (this.total - this.errors) +
+                    "; Failing: " + this.errors + ";");
+            System.out.println((this.errors == 0) ? " Perfect!" : "");
         } catch (Exception e) {
             System.err.println("Ocorreu um erro inesperado no sistema.\n" +
                     "É possível que o sistema esteja em um estado inconsistente.\n");
