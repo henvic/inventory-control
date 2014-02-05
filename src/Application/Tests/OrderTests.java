9f01d4efc224030d5daa47f0cc61dde7fdf82bb4
Henrique Vicente <henriquevicente@gmail.com>
2014-02-05 02:39:14 -0300

    Adding some Order tests
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- src/Application/Tests/OrderTests.java
+++ src/Application/Tests/OrderTests.java
@@ -10,8 +10,67 @@ public class OrderTests implements Tests {
     private Facade facade;
     private TestRunner testRunner;
 
+    public String createMockOrder() {
+        String buyer;
+        String seller;
+
+        try {
+            buyer = facade.createActor("Buyer #1",
+                    "Example Company",
+                    "foo@example.com",
+                    "+55 11 4444-4444",
+                    "5th Ave, 140 NYC",
+                    true,
+                    false);
+
+            seller = facade.createActor("Seller #2",
+                    "",
+                    "",
+                    "+55 88 8888-8888",
+                    "Cin, UFPE",
+                    false,
+                    true);
+        } catch (Exception ignore) {
+            System.out.println(ignore.getMessage());
+            return null;
+        }
+
+        try {
+            return facade.createOrder(buyer, seller);
+        } catch (Exception ignore) {
+        }
+
+        return null;
+    }
+
+
+    public Order readMockOrder(String id) {
+        try {
+            return facade.getOrder(id);
+        } catch (ObjectNotFoundException ignore) {
+        }
+
+        return null;
+    }
+
+    public boolean removeMockOrder(String id) {
+        try {
+            facade.removeOrder(id);
+            return true;
+        } catch (ObjectNotFoundException ignore) {
+        }
+
+        return false;
+    }
+
     public void runSuite() {
+        String temp;
+
         // @todo add missing tests
+        temp = this.createMockOrder();
+        testRunner.test(temp, "createMockOrder");
+        testRunner.test(readMockOrder(temp), "readMockOrder");
+        testRunner.test(removeMockOrder(temp), "removeMockOrder");
     }
 
     public OrderTests(Facade facade, TestRunner testRunner) {
